3994589040a19734c6eeb1bd8e903b8a24e56d80
Marius Bogoevici <mbogoevici@pivotal.io>
2017-06-12 01:02:41 -0400

    Sleep until ready

--- ticktock/env-init.sh
+++ ticktock/env-init.sh
@@ -6,6 +6,6 @@ tar xzvf kafka_2.12-0.10.2.1.tgz > out
 kafka_2.12-0.10.2.1/bin/zookeeper-server-start.sh kafka_2.12-0.10.2.1/config/zookeeper.properties > zookeeper.out  &
 kafka_2.12-0.10.2.1/bin/kafka-server-start.sh kafka_2.12-0.10.2.1/config/server.properties > kafka.out &
 
-nc -w120 -zv localhost 9092
+until nc -v -w 1 localhost 9092 2>nc.out; do sleep 1; done
 
 echo "Ready!"
