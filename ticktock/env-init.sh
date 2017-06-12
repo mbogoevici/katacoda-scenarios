a3b16a6808d4bc59b764edca1bdfcbff61d061ea
Marius Bogoevici <mbogoevici@pivotal.io>
2017-06-11 20:48:06 -0400

    Initial commit for ticktock

---
+++ ticktock/env-init.sh
@@ -0,0 +1,4 @@
+wget https://repo.spring.io/libs-release-local/org/springframework/cloud/spring-cloud-dataflow-server-local/1.2.1.RELEASE/spring-cloud-dataflow-server-local-1.2.1.RELEASE.jar
+wget https://repo.spring.io/libs-release-local/org/springframework/cloud/spring-cloud-dataflow-shell/1.2.1.RELEASE/spring-cloud-dataflow-shell-1.2.1.RELEASE.jar
+
+export SPRING_RABBITMQ_ADDRESSES=amqp://mhjrjlps:-Hckw-ANvR1r0hi8SDrj3uwYr96E-lvF@jellyfish.rmq.cloudamqp.com/mhjrjlps
