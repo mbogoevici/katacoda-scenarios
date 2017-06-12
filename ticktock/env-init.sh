5998ed9f9ee20cee0695ef6d6a847354b5fee345
Marius Bogoevici <mbogoevici@pivotal.io>
2017-06-11 23:27:07 -0400

    Add Rabbit

--- ticktock/env-init.sh
+++ ticktock/env-init.sh
@@ -1,2 +1,4 @@
 wget https://repo.spring.io/libs-release-local/org/springframework/cloud/spring-cloud-dataflow-server-local/1.2.1.RELEASE/spring-cloud-dataflow-server-local-1.2.1.RELEASE.jar
 wget https://repo.spring.io/libs-release-local/org/springframework/cloud/spring-cloud-dataflow-shell/1.2.1.RELEASE/spring-cloud-dataflow-shell-1.2.1.RELEASE.jar
+wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server-3.6.10.zip
+unzip rabbitmq-server-3.6.10.zip
