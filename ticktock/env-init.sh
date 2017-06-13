07bd313eff600e3c717647f5045ff78c1e277dba
Marius Bogoevici <mbogoevici@pivotal.io>
2017-06-13 12:23:00 -0400

    Wait for Zookeeper

--- ticktock/env-init.sh
+++ ticktock/env-init.sh
@@ -4,8 +4,11 @@ wget http://mirrors.koehn.com/apache/kafka/0.10.2.1/kafka_2.12-0.10.2.1.tgz > ou
 tar xzvf kafka_2.12-0.10.2.1.tgz > out
 
 kafka_2.12-0.10.2.1/bin/zookeeper-server-start.sh kafka_2.12-0.10.2.1/config/zookeeper.properties > zookeeper.out  &
+
+until nc -v -w 1 localhost 2181 2>nc.out; do sleep 1; 
+
 kafka_2.12-0.10.2.1/bin/kafka-server-start.sh kafka_2.12-0.10.2.1/config/server.properties > kafka.out &
 
-until nc -v -w 1 localhost 9092 2>nc.out; do sleep 1; 
+until nc -v -w 1 localhost 9092 2>nc.out; do sleep 1;
 
 echo "Ready!"
